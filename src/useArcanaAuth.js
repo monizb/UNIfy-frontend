f321298e8f9a69c9bee0d84ea25b25d6c6187828
Monish Basaniwal <monish2.basaniwal@gmail.com>
2023-02-05 20:09:43 +0530

    Added arcana

---
+++ src/useArcanaAuth.js
@@ -0,0 +1,71 @@
+import { AuthProvider } from "@arcana/auth";
+import { useEffect, useState } from "react";
+
+//Config
+const appAdd = '645d98c788a974b198d7c448992029a2920475e1';
+
+let auth = new AuthProvider(appAdd);
+
+function useArcanaAuth() {
+  const [initialized, setInitialized] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const initializeAuth = async () => {
+    await auth.init();
+    setInitialized(true);
+  };
+
+  //Social Login
+
+  const login = async (socialType) => {
+    if (initialized) {
+      await auth.loginWithSocial(socialType);
+      setLoggedIn(true);
+    }
+  };
+
+  //Email Link/ Passwordless login
+  const loginWithLink = async (email) => {
+    if (initialized) {
+      await auth.loginWithLink(email);
+      setLoggedIn(true);
+    }
+  };
+
+  //Getting user Accounts
+  const getAccounts = async () => {
+    if (initialized) {
+      return await auth.provider.request({ method: "eth_accounts" });
+    }
+  };
+
+  //Logout
+  const logout = async () => {
+    if (initialized && loggedIn) {
+      await auth.logout();
+      setLoggedIn(false);
+    }
+  };
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      await auth.init();
+      if (await auth.isLoggedIn()) {
+        setLoggedIn(true);
+      }
+    };
+    checkLogin();
+  }, []);
+
+  return {
+    initializeAuth,
+    loggedIn,
+    login,
+    loginWithLink,
+    getAccounts,
+    logout,
+    initialized,
+  };
+}
+
+export default useArcanaAuth;
